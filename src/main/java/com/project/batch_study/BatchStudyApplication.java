8e3368bb39666eb3e410fb31f8efe545cfa9011e
yerin-158 <hanblueblue@gmail.com>
2020-06-29 16:35:02 +0900

    init

---
+++ src/main/java/com/project/batch_study/BatchStudyApplication.java
@@ -0,0 +1,13 @@
+package com.project.batch_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/batch_study/BatchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.batch_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
