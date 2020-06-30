26167b08bfa41aad99a4dcc6f39105e3cbb984cb
yerin-158 <hanblueblue@gmail.com>
2020-06-30 15:38:38 +0900

    임의 데이터 추가용 코드 추가

--- src/test/java/com/project/batch_study/domain/PayRepositoryTest.java
+++ src/test/java/com/project/batch_study/domain/PayRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.project.batch_study.domain;
 
 import com.project.batch_study.utils.TestUtil;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -33,6 +36,24 @@ class PayRepositoryTest {
         payRepository.save(newData);
 
         assertThat(Optional.ofNullable(newData.getId()).isPresent(), equalTo(true));
+    }
+
+    @Test
+    @DisplayName("배치에 사용할 데이터 추가용. 테스트 코드 아님")
+    void insertData(){
+        List<Pay> pays = new ArrayList<>();
+
+        for (int i=0; i<100; ++i){
+            Pay newData = Pay.builder()
+                    .txName(TestUtil.getRandomString(10))
+                    .DateTime(LocalDateTime.now())
+                    .amount(TestUtil.getRandomLong(1000, 100000))
+                    .build();
+
+            pays.add(newData);
+        }
+
+        payRepository.saveAll(pays);
     }
 
 }
