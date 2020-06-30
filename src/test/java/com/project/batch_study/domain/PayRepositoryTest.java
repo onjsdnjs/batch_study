ecde4abe831629c125a14681363f716fd64b0433
yerin-158 <hanblueblue@gmail.com>
2020-06-30 16:51:42 +0900

    Pay Entity insert 시에 새로 추가된 컬럼인 successStatus를 포함하도록 변경

--- src/test/java/com/project/batch_study/domain/PayRepositoryTest.java
+++ src/test/java/com/project/batch_study/domain/PayRepositoryTest.java
@@ -9,14 +9,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -26,11 +24,12 @@ class PayRepositoryTest {
     private PayRepository payRepository;
 
     @Test
-    void insertTest(){
+    void insertTest() {
         Pay newData = Pay.builder()
                 .txName(TestUtil.getRandomString(10))
                 .DateTime(LocalDateTime.now())
                 .amount(TestUtil.getRandomLong(1000, 100000))
+                .successStatus(false)
                 .build();
 
         payRepository.save(newData);
@@ -40,14 +39,20 @@ class PayRepositoryTest {
 
     @Test
     @DisplayName("배치에 사용할 데이터 추가용. 테스트 코드 아님")
-    void insertData(){
+    void insertData() {
         List<Pay> pays = new ArrayList<>();
 
-        for (int i=0; i<100; ++i){
+        for (int i = 0; i < 100; ++i) {
+            boolean successStatus = true;
+            if (i % 3 == 0) {
+                successStatus = false;
+            }
+
             Pay newData = Pay.builder()
                     .txName(TestUtil.getRandomString(10))
                     .DateTime(LocalDateTime.now())
                     .amount(TestUtil.getRandomLong(1000, 100000))
+                    .successStatus(successStatus)
                     .build();
 
             pays.add(newData);
