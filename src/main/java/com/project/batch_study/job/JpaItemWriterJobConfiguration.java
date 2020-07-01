3187d0b9d692ef6c3df6cd95426a59c8c1f31daf
yerin-158 <hanblueblue@gmail.com>
2020-07-01 20:56:25 +0900

    JpaItemWriter를 사용해 데이터 복제해 저장하기

---
+++ src/main/java/com/project/batch_study/job/JpaItemWriterJobConfiguration.java
@@ -0,0 +1,79 @@
+package com.project.batch_study.job;
+
+import com.project.batch_study.domain.Pay;
+import com.project.batch_study.domain.PayDuplicate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+@ConditionalOnProperty(name = "job.name", havingValue = JpaItemWriterJobConfiguration.JOB_NAME)
+public class JpaItemWriterJobConfiguration {
+    public static final String JOB_NAME = "jpaItemWriterJob";
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+    private static final int chunkSize = 10;
+
+    @Bean
+    public Job jpaItemWriterJob() {
+        return jobBuilderFactory.get(JOB_NAME)
+                .start(jpaItemWriterStep())
+                .build();
+    }
+
+    @Bean
+    public Step jpaItemWriterStep() {
+        return stepBuilderFactory.get("jpaItemWriterStep")
+                .<Pay, PayDuplicate>chunk(chunkSize)
+                .reader(jpaItemWriterReader())
+                .processor(jpaItemProcessor())
+                .writer(jpaItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JpaPagingItemReader<Pay> jpaItemWriterReader() {
+        return new JpaPagingItemReaderBuilder<Pay>()
+                .name("jpaItemWriterReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(chunkSize)
+                .queryString("SELECT p FROM Pay p")
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<Pay, PayDuplicate> jpaItemProcessor(){
+        return pay -> PayDuplicate.builder()
+                .txName(pay.getTxName())
+                .amount(pay.getAmount())
+                .DateTime(pay.getTxDateTime())
+                .successStatus(pay.isSuccessStatus())
+                .build();
+    }
+
+    @Bean
+    public JpaItemWriter<PayDuplicate> jpaItemWriter(){
+        return new JpaItemWriterBuilder<PayDuplicate>()
+                .entityManagerFactory(entityManagerFactory)
+                .build();
+    }
+
+}
