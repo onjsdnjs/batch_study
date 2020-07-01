db42ccb56722cc1550940fce4a5c1fb19e0f97af
yerin-158 <hanblueblue@gmail.com>
2020-07-01 18:33:15 +0900

    jpa페이징을 사용하나 limit의 인덱스를 0으로 설정해 데이터를 뛰어넘지 못하게 한다.

---
+++ src/main/java/com/project/batch_study/job/PayPagingJobConfiguration.java
@@ -0,0 +1,89 @@
+package com.project.batch_study.job;
+
+import com.project.batch_study.domain.Pay;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+@ConditionalOnProperty(name = "job.name", havingValue = PayPagingJobConfiguration.JOB_NAME)
+public class PayPagingJobConfiguration {
+
+    public static final String JOB_NAME = "payPagingSuccessJob";
+
+    private final EntityManagerFactory entityManagerFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final int chunkSize = 10;
+
+    @Bean
+    public Job payPagingJob() {
+        return jobBuilderFactory.get(JOB_NAME)
+                .start(payPagingStep())
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step payPagingStep() {
+        return stepBuilderFactory.get("payPagingStep")
+                .<Pay, Pay>chunk(chunkSize)
+                .reader(payPagingReader())
+                .processor(payPagingProcessor())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public JpaPagingItemReader<Pay> payPagingReader() {
+        JpaPagingItemReader<Pay> reader = new JpaPagingItemReader<Pay>() {
+            @Override
+            public int getPage() {
+                return 0;
+            }
+        };
+
+        reader.setQueryString("SELECT p FROM Pay p WHERE p.successStatus = false");
+        reader.setPageSize(chunkSize);
+        reader.setEntityManagerFactory(entityManagerFactory);
+        reader.setName("payPagingReader");
+
+        return reader;
+    }
+
+    @Bean
+    @StepScope
+    public ItemProcessor<Pay, Pay> payPagingProcessor() {
+        return item -> {
+            item.success();
+            return item;
+        };
+    }
+
+    @Bean
+    @StepScope
+    public JpaItemWriter<Pay> writer() {
+        JpaItemWriter<Pay> writer = new JpaItemWriter<>();
+        writer.setEntityManagerFactory(entityManagerFactory);
+        return writer;
+    }
+
+}
