c01aa2288f5f26c5370f1f55539f107e56ab4815
yerin-158 <hanblueblue@gmail.com>
2020-07-02 20:32:15 +0900

    writer가 하나의 트랜잭션에 포함되어 있으므로 Lazy 로딩에 성공하는 것을 확인한다.

---
+++ src/main/java/com/project/batch_study/job/TransactionWriterJobConfiguration.java
@@ -0,0 +1,72 @@
+package com.project.batch_study.job;
+
+import com.project.batch_study.domain.Teacher;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+@ConditionalOnProperty(name = "job.name", havingValue = TransactionWriterJobConfiguration.JOB_NAME)
+public class TransactionWriterJobConfiguration {
+    public static final String JOB_NAME = "transactionWriterBatch";
+    public static final String BEAN_PREFIX = JOB_NAME + "_";
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory emf;
+
+    private final int chunkSize = 10;
+
+    @Bean(JOB_NAME)
+    public Job job() {
+        return jobBuilderFactory.get(JOB_NAME)
+                .preventRestart()
+                .start(step())
+                .build();
+    }
+
+    @Bean(BEAN_PREFIX + "step")
+    @JobScope
+    public Step step() {
+        return stepBuilderFactory.get(BEAN_PREFIX + "step")
+                .<Teacher, Teacher>chunk(chunkSize)
+                .reader(reader())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    public JpaPagingItemReader<Teacher> reader() {
+        return new JpaPagingItemReaderBuilder<Teacher>()
+                .name(BEAN_PREFIX+"reader")
+                .entityManagerFactory(emf)
+                .pageSize(chunkSize)
+                .queryString("SELECT t FROM Teacher t")
+                .build();
+    }
+
+    private ItemWriter<Teacher> writer() {
+        return items -> {
+            log.info(">>>>>>>>>>> Item Write");
+            for (Teacher item : items) {
+                log.info("teacher={}, student Size={}", item.getName(), item.getStudents().size());
+            }
+        };
+    }
+
+
+}
